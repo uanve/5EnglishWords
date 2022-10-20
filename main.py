5185665bf400a922a9409666096b396e863c9a82
uanve <joanventuraomella@gmail.com>
2022-10-20 19:01:38 +0200

    update changes

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ all_words = pd.read_csv("https://github.com/dwyl/english-words/raw/master/words_
 
 
 # Parameters
-N_words = 5
+N_generated_words = 3
+N_words = 5000
 
 words = [w for w in all_words.values if len(w)==5][:]
 random.shuffle(words)
 
-words = words[:]
+words = words[:N_words]
 
 
 abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -36,37 +37,37 @@ model = cp_model.CpModel()
 # The array index is the column, and the value is the row.
 
 chosen_word_var = {}
-for k in range(N_words):
+for k in range(N_generated_words):
     for w in range(len(words)):
         chosen_word_var[k,w] = model.NewBoolVar('word_{}_{}'.format(k,w)) 
 
 
 word_var={}  
-for k in range(N_words):
+for k in range(N_generated_words):
     for i in range(5):
         for j in range(len(abc)):    
             word_var[k,i,j] = model.NewBoolVar("w_{}_{}_{}".format(k,i,j))
 
 
 #define a word
-for k in range(N_words):
+for k in range(N_generated_words):
     for j in range(len(abc)):
         model.Add( sum(word_var[k,i,j] for i in range(5)) <= 1 )
 
 #if word i is chosen: then variable must be the same as chosen word
-for k in range(N_words):
+for k in range(N_generated_words):
     for w in range(len(words)):
         for i in range(5):
             for j in range(len(abc)):   
                 model.Add( word_var[k,i,j] == word_to_numb(words[w])[j][i]).OnlyEnforceIf(chosen_word_var[k,w])
              
 #choose one word
-for k in range(N_words):
+for k in range(N_generated_words):
     model.Add( sum(chosen_word_var[k,w] for w in range(len(words))) == 1 )
     
 #Cannot repeat a letter
 for j in range(len(abc)):
-    model.Add( sum(word_var[k,i,j] for k in range(N_words) for i in range(5)) <=1 )
+    model.Add( sum(word_var[k,i,j] for k in range(N_generated_words) for i in range(5)) <=1 )
 
             
              
@@ -77,7 +78,7 @@ status = solver.Solve(model)
 print(status==cp_model.OPTIMAL)
 
 result_words_index = []
-for k in range(N_words):
+for k in range(N_generated_words):
     for i in range(len(words)):
         if solver.Value(chosen_word_var[k,i])==1:
             result_words_index.append(i)
@@ -85,7 +86,7 @@ for k in range(N_words):
             
         
 
-for k in range(N_words):
+for k in range(N_generated_words):
     result_word = ""        
     for i in range(5):
         for j in range(len(abc)):    
